a3b217279cda24e6358e1febdf7049ff36b68a1f
a <a@a.com>
2026-09-02 04:16:58 -0500

    works in testnet

--- config.ts
+++ config.ts
@@ -5,11 +5,26 @@ export const CONFIG = {
   mainnet: {
     network: "mainnet",
     baseUrl: "https://fullnode.mainnet.sui.io:443",
-    usdc: "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
   },
   testnet: {
     network: "testnet",
     baseUrl: "https://fullnode.testnet.sui.io:443",
-    usdc: "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC",
   },
 };
+
+export const TOKENS = {
+  "sui": {
+    address: {
+      "mainnet": "0x2::sui::SUI",
+      "testnet": "0x2::sui::SUI"
+    },
+    decimals: 9
+  },
+  "usdc": {
+    address: {
+      "mainnet": "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
+      "testnet": "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC"
+    },
+    decimals: 6
+  }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import "@mysten/dapp-kit-core/web";
 import { createDAppKit } from "@mysten/dapp-kit-core";
 import { SuiGrpcClient } from "@mysten/sui/grpc";
-import { RECIPIENT, CONFIG } from "./config";
+import { Transaction } from "@mysten/sui/transactions";
+import { RECIPIENT, CONFIG, SUI, TOKENS } from "./config";
 
 const dAppKit = createDAppKit({
   networks: ["testnet", "mainnet"],
@@ -19,6 +20,8 @@ button.instance = dAppKit;
 
 const tipUI = document.getElementById("tipUI");
 const tipAmounts = document.getElementById("tipAmounts");
+const tipTokenSelect = document.getElementById("tipToken");
+const submitTipButton = document.getElementById("submitTip");
 
 function toggleTipUI() {
   const connection = dAppKit.stores.$connection.get();
@@ -55,6 +58,7 @@ function addTipAmounts(amounts) {
   let customInputField = document.createElement("input");
   customInputField.type = "number";
   customInputField.placeholder = "Enter custom amount";
+  customInputField.id = "customTipAmountInput";
 
   let customInputRadio = document.createElement("input");
   customInputRadio.type = "radio";
@@ -72,9 +76,83 @@ function addTipAmounts(amounts) {
   tipAmounts.append(container);
 }
 
+function toBaseUnits(amount: string, decimals: number): bigint {
+  const [whole, fraction = ""] = amount.split(".");
+
+  if (fraction.length > decimals) {
+    throw new Error(`Too many decimal places. Maximum is ${decimals}`);
+  }
+
+  return (
+    BigInt(whole) * 10n ** BigInt(decimals) +
+    BigInt(fraction.padEnd(decimals, "0"))
+  );
+}
+
+async function sendToken(senderAddress, token, amountRaw) {
+  const network = dAppKit.stores.$currentNetwork.get();
+
+  if (!network) {
+    return;
+  }
+
+  const tokenAddress = token.address[network];
+  const amount = toBaseUnits(amountRaw, token.decimals);
+
+  const tx = new Transaction();
+  tx.setSender(senderAddress);
+
+  tx.moveCall({
+    target: "0x2::balance::send_funds",
+    typeArguments: [tokenAddress],
+    arguments: [
+      tx.balance({
+        type: tokenAddress,
+        balance: amount,
+      }),
+      tx.pure.address(RECIPIENT),
+    ],
+  });
+
+  const result = await dAppKit.signAndExecuteTransaction({
+    transaction: tx,
+  });
+
+  if (result.FailedTransaction) {
+    throw new Error(
+      `Transaction failed: ${result.FailedTransaction.status.error?.message}`
+    );
+  }
+  console.log("Transaction digest:", result.Transaction.digest);
+}
+
+function submitTip() {
+  const connection = dAppKit.stores.$connection.get();
+  const senderAddress = connection?.account?.address;
+
+  if (!senderAddress) {
+    return;
+  }
+
+  const selectedTipAmountRadio = document.querySelector(
+    'input[name="tipAmount"]:checked'
+  );
+  let tipAmountRaw = selectedTipAmountRadio.value;
+  if (tipAmountRaw === "custom") {
+    tipAmountRaw = document.getElementById("customTipAmountInput").value;
+  }
+
+  const tokenKey = tipTokenSelect.value;
+  const token = TOKENS[tokenKey];
+
+  sendToken(senderAddress, token, tipAmountRaw);
+}
+
 const unsubscribe = dAppKit.stores.$connection.subscribe((connection) => {
   toggleTipUI();
 });
 
 addTipAmounts([0.01, 0.05, 0.1, 0.25, 1, 5]);
 toggleTipUI();
+
+submitTipButton.addEventListener("click", submitTip);
